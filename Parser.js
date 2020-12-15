28f0a311207d4d69e316a25c27446a81bd71c169
Anirudh Sharma <68021981+anirudhforhire20@users.noreply.github.com>
2020-12-15 16:24:40 +0530

    Add files via upload

---
+++ Parser.js
@@ -0,0 +1,106 @@
+//This script parses the equation
+//Two entites can be added, subtracted, multiplied 
+//or divided if they share the same parent in a given
+//hierarchy level
+//for example
+//  6*(3 + 2)
+//here, 6 and (3 + 2) share the same parent on the same level
+//but 6 is a number and (3 + 2) is technically a term
+// 3 and 2 are children of given term
+//Condiiton 1 is similar types can only be operated(directly)
+//6 is not similar to (3 + 2) but 5 and 6 are similar
+//we need to parse the children of (3 + 2) in order to figure\
+//out which operation or series of operations you should perform
+//6*(((3/2) + 2^2)/(4 + 7)) can be considered to contain
+//various nested children of multiple types
+
+//Here are rules for relation between different types
+// numb(a)*frac(num(numb(b))/den(numb(c))) = frac(num(numb(a*b))/den(numb(c)))    number*numerator/den
+
+//numb(a)*frac(num(term(b))/den(numb(c))) = frac(num(term(a.b))/den(numb(c)))     distrubtive property
+
+// frac(a)*frac(b) = frac(a1*b1/a2*b2) 
+//frac(a)+frac(b) apply laws of addition of fractions
+
+//fractions => numbers or terms by dividing denominator with each number in numerator term
+
+
+function CalculateAnimations(ele, floating)
+{
+    if(floating != null)
+    {
+        if(ele.classList.contains('operator'))
+        {
+            ele.onmouseenter = function() {
+                this.parentElement.style.backgroundColor = 'grey';
+                /*for(var ele of this.parentElement.children)
+                {
+                    ele.style.backgroundColor = 'grey';
+                }*/
+            }
+            ele.onmouseleave = function() {
+                this.parentElement.style.backgroundColor = 'rgba(255,255,255,0)';
+                /*for(var ele of this.parentElement.children)
+                {
+                    ele.style.backgroundColor = 'white';
+                }*/
+            }
+        }
+        /*else if((floating.parentElement.classList.contains('den') && (['num', 'term'] in ele.parentElement.classList || ele.classList.contains('numb'))) || ((['num', 'term'] in floating.parentElement.classList || floating.classList.contains('numb')) && ele.parentElement.classList.contains('den')))
+        {
+
+            var clone = ele.cloneNode(true);
+            ele.parentElement.replaceChild(clone, ele);
+        }*/
+        else if(floating.parentElement === ele.parentElement)
+        {
+            ele.onmouseenter = function() {
+                this.style.backgroundColor = 'grey';
+            }
+            ele.onmouseleave = function() {
+                this.style.backgroundColor = 'rgba(255,255,255,0)';
+            }
+        }
+        else if(ele.parentElement.parentElement === floating.parentElement.parentElement)
+        {
+            if(ele.classList.contains("num") || ele.classList.contains("den"))
+            {
+                ele.onmouseenter = function() {
+                    this.style.backgroundColor = 'grey';
+                }
+                ele.onmouseleave = function() {
+                    this.style.backgroundColor = 'rgba(255,255,255,0)';
+                }   
+            }
+        }
+    }
+    else
+    {
+        if(ele.classList.contains('operator'))
+        {
+            ele.onmouseenter = function() {
+                this.parentElement.style.backgroundColor = 'grey';
+                /*for(var ele of this.parentElement.children)
+                {
+                    ele.style.backgroundColor = 'grey';
+                }*/
+            }
+            ele.onmouseleave = function() {
+                this.parentElement.style.backgroundColor = 'rgba(255,255,255,0)';
+                /*for(var ele of this.parentElement.children)
+                {
+                    ele.style.backgroundColor = 'white';
+                }*/
+            }
+        }
+        else
+        {
+            ele.onmouseenter = function() {
+                this.style.backgroundColor = 'grey';
+            }
+            ele.onmouseleave = function() {
+                this.style.backgroundColor = 'rgba(255,255,255,0)';
+            }
+        }
+    }
+}
---
+++ Renderer.js
@@ -0,0 +1,100 @@
+//React js is trash so here's everything react can do, better
+var position = {
+    x : 0,
+    y : 0
+};
+var float_el = null;
+function Calculate()
+{
+    var elements = [];
+    for(var ele of document.getElementsByClassName('numb'))
+    {
+        ele.onmousedown = function(event) {
+            const el = this;
+            //console.log('down');
+            float_el = this;
+            var copy = this.cloneNode(true);
+            for(var e of document.getElementsByClassName('numb'))
+            {
+                CalculateAnimations(e, float_el);
+            }
+            for(var e of document.getElementsByClassName('operator'))
+            {
+                CalculateAnimations(e, float_el);
+            }
+            copy.id = "floating"
+            this.style.visibility = 'hidden';
+            copy.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+            this.style.position = 'absolute';
+            copy.style.position = 'absolute';
+            copy.style.left = window.getComputedStyle(el).left;
+            copy.style.top = window.getComputedStyle(el).top;
+            //position.x = -parseFloat(copy.style.left.split('px')[0]) + parseFloat(event.clientX);
+            //position.y = -parseFloat(copy.style.top.split('px')[0]) + parseFloat(event.clientY);
+            position.x = -parseFloat(window.getComputedStyle(el).left.split('px')[0]) + parseFloat(event.clientX);
+            position.y = -parseFloat(window.getComputedStyle(el).top.split('px')[0]) + parseFloat(event.clientY);
+            //console.log(position)
+            this.style.position = 'relative';
+            document.body.append(copy);
+            //event.preventDefault();
+        }
+        CalculateAnimations(ele, float_el);
+    }
+    for(var ele of document.getElementsByClassName('operator'))
+    {
+        ele.onmousedown = function(event) {
+            const el = this;
+            //console.log('down');
+            float_el = this.parentElement;
+            for(var e of document.getElementsByClassName('numb'))
+            {
+                CalculateAnimations(e, float_el);
+            }
+            for(var e of document.getElementsByClassName('operator'))
+            {
+                CalculateAnimations(e, float_el);
+            }
+            var copy = this.parentElement.cloneNode(true);
+            copy.id = "floating"
+            this.parentElement.style.visibility = 'hidden';
+            copy.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+            this.parentElement.style.position = 'absolute';
+            copy.style.position = 'absolute';
+            copy.style.left = window.getComputedStyle(el.parentElement).left;
+            copy.style.top = window.getComputedStyle(el.parentElement).top;
+            position.x = -parseFloat(window.getComputedStyle(el.parentElement).left.split('px')[0]) + parseFloat(event.clientX);
+            position.y = -parseFloat(window.getComputedStyle(el.parentElement).top.split('px')[0]) + parseFloat(event.clientY);
+            //position.x = parseFloat(copy.style.width.split('px')[0])/2;
+            //position.x = parseFloat(window.copy.style.height.split('px')[0])/2;
+            //console.log(position)
+            this.parentElement.style.position = 'relative';
+            document.body.append(copy);
+            //event.preventDefault();
+        }
+        CalculateAnimations(ele, float_el);
+    }
+}
+
+document.body.onmousemove = function(event) {
+    event.preventDefault();
+    if(document.getElementById('floating'))
+    {
+        //console.log('moving');
+        document.getElementById('floating').style.left = (event.clientX - position.x).toString() + 'px';
+        document.getElementById('floating').style.top = (event.clientY - position.y).toString() + 'px';
+        //document.getElementById('floating').blur();
+    }
+}
+document.body.onmouseup = function(event) {
+    event.preventDefault();
+    //console.log('up');
+    if(document.getElementById('floating'))
+    {
+        document.getElementById('floating').innerHTML = "";
+        document.getElementById('floating').remove();
+    } 
+    float_el.style.visibility = 'visible';
+    float_el = null;
+    Calculate();
+}
+Calculate();
